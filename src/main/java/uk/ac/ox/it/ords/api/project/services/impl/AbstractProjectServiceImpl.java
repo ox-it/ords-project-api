f798242c2e877a217209fa9e692ae6909346a951
ouit0005 <ouit0005@87df6a52-bba1-4346-a3e1-e27aa7044e28>
2015-10-21 11:36:35 +0000

    Added license headers to source files

--- src/main/java/uk/ac/ox/it/ords/api/project/services/impl/AbstractProjectServiceImpl.java
+++ src/main/java/uk/ac/ox/it/ords/api/project/services/impl/AbstractProjectServiceImpl.java
@@ -1,6 +1,20 @@
+/*
+ * Copyright 2015 University of Oxford
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package uk.ac.ox.it.ords.api.project.services.impl;
 
-import org.apache.shiro.SecurityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,10 +30,11 @@ public abstract class AbstractProjectServiceImpl implements ProjectService{
 	/**
 	 * Configures the project with internal system-set properties; this is called once on 
 	 * project creation.
+	 * TODO use ServerDetailsService to populate
 	 * @param project
 	 */
 	public void configureProject(Project project){
-		// TODO use ServerDetailsService to populate
+		
 		project.setDbServerAddress("localhost");
 	}
 	
