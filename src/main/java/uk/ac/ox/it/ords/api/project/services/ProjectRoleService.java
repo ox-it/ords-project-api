7c13ce2d49f01b705e7c37e0158fc545ff3e5038
ouit0005 <ouit0005@87df6a52-bba1-4346-a3e1-e27aa7044e28>
2015-10-21 09:53:57 +0000

    added Javadoc

--- src/main/java/uk/ac/ox/it/ords/api/project/services/ProjectRoleService.java
+++ src/main/java/uk/ac/ox/it/ords/api/project/services/ProjectRoleService.java
@@ -8,22 +8,56 @@ import uk.ac.ox.it.ords.api.project.services.impl.hibernate.ProjectRoleServiceIm
 
 public interface ProjectRoleService {
 	
+	/**
+	 * Create the Owner role and their permissions; called once when a new project is created
+	 * @param projectId
+	 * @throws Exception
+	 */
 	public void createInitialPermissions(int projectId) throws Exception;
+	
+	/**
+	 * Delete all the permissions and roles associated with a project; called once when a project is deleted
+	 * @param projectId
+	 * @throws Exception
+	 */
 	public void deletePermissions(int projectId) throws Exception;
 	
 	/**
 	 * Return all the UserRoles that match the pattern of the project
 	 * @param projectId
-	 * @return
+	 * @return a List of UserRole objects
 	 * @throws Exception
 	 */
 	public List<UserRole> getUserRolesForProject(int projectId) throws Exception;
 	
+	/**
+	 * Return the specified UserRole instance
+	 * @param roleId 
+	 * @return the UserRole specified, or null if there is no match
+	 * @throws Exception
+	 */
 	public UserRole getUserRole(int roleId) throws Exception;
-	public UserRole addUserRoleToProject(int projectid, UserRole userRole) throws Exception;
-	public void removeUserFromRoleInProject(int projectid, int roleId) throws Exception;
 	
+	/**
+	 * Create the UserRole 
+	 * @param projectid
+	 * @param userRole
+	 * @return the UserRole that has been persisted
+	 * @throws Exception
+	 */
+	public UserRole addUserRoleToProject(int projectid, UserRole userRole) throws Exception;
+	
+	/**
+	 * Remove the UserRole
+	 * @param projectid
+	 * @param roleId
+	 * @throws Exception
+	 */
+	public void removeUserFromRoleInProject(int projectid, int roleId) throws Exception;	
 
+	/**
+	 * The enumeration of valid UserRole types
+	 */
     public enum ProjectRole {
         owner, projectadministrator, contributor, viewer, deleted
     };
